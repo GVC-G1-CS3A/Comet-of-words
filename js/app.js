f9021eb93bb93d316d84b5f43985389ae9734afd
Jmr13 <Gamayanjomar@yahoo.com>
2022-01-08 17:56:16 +0800

    Minor changes
    Description
    Clear input box

--- js/app.js
+++ js/app.js
@@ -283,10 +283,12 @@ function resetGame() {
     }
 
     pointingElementID.innerHTML = "";
+    inputElementID.value = '';
     scoreElementID.innerHTML = `<p>Score: 0</p>`;
 
     totalSeconds = (startingMinutes * 60) - 1;
     countDownInterval = setInterval(updateCountdown, 1000); // game begins
+
 }
 
 
@@ -307,6 +309,7 @@ window.onload = function () {
     asteroid5.src = './images/asteroid-5.png';
 
     mainLoop();
+
 }
 
 function clear(canvas, fillstyle) {
